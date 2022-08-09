ab5552c334e251e22cf1b1d74be291ea78cf09aa
PhillipsOG <connorphillips199612@gmail.com>
2022-08-09 15:57:16 +0100

    VersaServer v0.1 template.

---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace VersaServer
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "VersaServer";
+        public const string PLUGIN_NAME = "VersaServer";
+        public const string PLUGIN_VERSION = "2020.3.31";
+    }
+}
